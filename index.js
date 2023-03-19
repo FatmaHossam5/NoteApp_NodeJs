1df7e5cc31e0eb07fe6649a0dec367b90c9697af
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2023-03-19 23:19:32 +0200

    define port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import {appRouter} from "./modules/index.router.js"
 dotenv.config()
-
+const port = process.env.PORT 
 const app = express()
 appRouter(app)
 

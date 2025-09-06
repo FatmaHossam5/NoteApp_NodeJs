d030e1c8829ab164b98df531a7dba25d74bbeb67
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2025-09-06 00:30:18 -0400

    Refactor note router: Improved route definitions by adding a new endpoint for retrieving a note by ID, updated user notes endpoint, and ensured consistent import formatting.

--- modules/note/note.router.js
+++ modules/note/note.router.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-const router =Router()
-import {auth} from '../../middleware/auth.js'
-import {validation} from '../../middleware/validation.js'
-import * as validator from './note.validation.js'
- import * as noteController from './controller/note.js'
- router.post("/add",auth(),validation(validator.addNoteValidate),noteController.addNote)
- router.get("/notes/:id",auth(),validation(validator.NoteValidate),noteController.userNotes)
- router.put('/update/:id',auth(),validation(validator.updateNoteValidate),noteController.updateNote)
- router.delete('/delete/:id',auth(),validation(validator.NoteValidate),noteController.deleteNote)
+const router = Router();
+import { auth } from '../../middleware/auth.js';
+import { validation } from '../../middleware/validation.js';
+import * as validator from './note.validation.js';
+import * as noteController from './controller/note.js';
+
+router.post("/add", auth(), validation(validator.addNoteValidate), noteController.addNote);
+router.get("/notes", auth(), noteController.userNotes);
+router.get("/notes/:id", auth(), validation(validator.NoteValidate), noteController.getNoteByID);
+router.put('/update/:id', auth(), validation(validator.updateNoteValidate), noteController.updateNote);
+router.delete('/delete/:id', auth(), validation(validator.NoteValidate), noteController.deleteNote);
 
 
 export default router
